f88e39a5a24a59beb7b84ba61cfb82b29f10b44a
B.Ding <742819176@qq.com>
2018-07-20 07:40:39 +0800

    Initial Commit

---
+++ MYTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYTimer
+//
+//  Created by dingbinbin on 2018/7/20.
+//  Copyright © 2018年 dingbinbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
